ad0686a6b7e64790cc9abb50eb5cb3eaf8724e4c
Benjamin Ding <ben@dingbenjamin.com>
2018-05-28 22:06:21 +0200

    Remove MSP432/CCS/TI specific folders/files

--- Debug/CMSIS/DSP_Lib/Source/TransformFunctions/arm_bitreversal2.d
+++
@@ -1,5 +0,0 @@
-# FIXED
-
-CMSIS/DSP_Lib/Source/TransformFunctions/arm_bitreversal2.obj: ../CMSIS/DSP_Lib/Source/TransformFunctions/arm_bitreversal2.S
-
-../CMSIS/DSP_Lib/Source/TransformFunctions/arm_bitreversal2.S: 
--- Release/extraction/loop.d
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-extraction/loop.obj: ../extraction/loop.c
-extraction/loop.obj: ../extraction/loop.h
-extraction/loop.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_17.6.0.STS/include/stdint.h
-
-../extraction/loop.c: 
-../extraction/loop.h: 
-C:/ti/ccsv7/tools/compiler/ti-cgt-arm_17.6.0.STS/include/stdint.h: 
