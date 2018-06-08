92cf37e5c257ef9ae253497f1469a4d87c8d73ea
Benjamin Ding <ben@dingbenjamin.com>
2018-06-09 00:42:36 +0200

    Write code generator for test data

---
+++ fann_generation/fann_data.h
@@ -0,0 +1,8 @@
+#ifndef FANN_FANN_TEST_DATA_H_
+#define FANN_FANN_TEST_DATA_H_
+
+extern fann_type test_data_input[17800];
+
+extern const int test_data_output[100];
+
+#endif // FANN_FANN_TEST_DATA_H_
