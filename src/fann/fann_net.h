4584ba614337018bdc65ad59405339d3e6af882b
Benjamin Ding <ben@dingbenjamin.com>
2018-06-08 23:09:33 +0200

    Write code generator for keras to fann

--- src/fann/fann_net.h
+++ src/fann/fann_net.h
@@ -6,11 +6,10 @@
 
 extern const enum fann_nettype_enum network_type;
 
-extern fann_neuron fann_neurons[112];
+extern fann_neuron fann_neurons[356];
 
-extern fann_type fann_weights[3003];
+extern fann_type fann_weights[15930];
 
 extern fann_layer fann_layers[4];
 
-
-#endif // FANN_FANN_NET_H_
+#endif // FANN_FANN_NET_H
