ea1bc28cdaaff02c874d643f8d453585bbe6cd57
Benjamin Ding <ben@dingbenjamin.com>
2018-06-09 22:16:41 +0200

    Achieve 95% accuracy

--- src/definitions.h
+++ src/definitions.h
@@ -23,14 +23,14 @@
 #ifdef TEST_EPILEPSY
 	#include "../data/epilepsy_data.h"
 	#define NUM_TEST_SAMPLES 100
-	#define NUM_NEURONS 271
+	#define NUM_NEURONS 234
 	#define MULTIPLIER 2
 	#define DECIMAL_POINT 1
 	#define NUM_INPUT 178
 	#define NUM_OUTPUT 2
-	#define NUM_LAYERS 3
-	#define NUM_WEIGHTS 15930
+	#define NUM_LAYERS 4
 	#define CONNECTION_RATE 1.000000
+	#define NUM_WEIGHTS 7592
 #endif
 
 #ifdef TEST_FEATURE_EXTRACTION
